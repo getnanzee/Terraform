746b809d134e2c5f5906e4fea458972a3fcdf004
Abhishek Sharma <bizz.abby@gmail.com>
2019-12-08 00:59:16 +0530

    Extended root volume
    Extended root volume to 30 GB

--- instances.tf
+++ instances.tf
@@ -2,6 +2,10 @@ resource "aws_instance" "elastic-master" {
   ami = "${lookup(var.AMIS, var.REGION)}"
   instance_type = "t2.micro"
 
+  root_block_device {
+      volume_size = 30
+  }
+
   key_name = "${aws_key_pair.elastickey.key_name}"
 
   connection {
@@ -14,13 +18,16 @@ resource "aws_instance" "elastic-node-1" {
   ami = "${lookup(var.AMIS, var.REGION)}"
   instance_type = "t2.micro"
 
+  root_block_device {
+      volume_size = 30
+  }
+
   key_name = "${aws_key_pair.elastickey.key_name}"
 
   connection {
       user = "${var.INSTANCE_USER}"
       private_key = "${file("${var.PRIVATE_KEY}")}"
   }
-
 }
 
 resource "aws_key_pair" "elastickey" {
